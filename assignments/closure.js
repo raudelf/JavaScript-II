a6dd9ebf1aa7d5b59baaee5ce106b38e46d2c655
Raudel Flores <rawdell@outlook.com>
2019-10-07 19:00:12 -0500

    finished closures challenge

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,7 +3,13 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
-
+let favFood = () => {
+  let food = 'Pizza'
+  return sentence = () => {
+    return `I like ${food}`;
+  }
+}
+console.log(favFood()());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
